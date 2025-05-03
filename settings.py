cc0b19514f72aff284cc728578c485c02deba23f
dennisg54 <dgellegani@student.cscc.edu>
2025-05-03 18:52:09 -0400

    split settings into dynamic and static added increase difficulty function

--- settings.py
+++ settings.py
@@ -14,6 +14,7 @@ class Settings:
         self.screen_w: int = 1200
         self.screen_h: int = 800
         self.FPS = 60
+        self.difficulty_scale = 1.1
         
         # Initialize the game background
         """
@@ -32,15 +33,9 @@ class Settings:
         self.ship_file = Path.cwd() / "Assets" / "images" / "starship(nobg).png"
         self.ship_w = 40
         self.ship_h = 60
-        self.ship_speed = 5
-        self.starting_ship_amount = 3
         
         # Initialize the game bullet settings - the player's ship bullets
         self.bullet_file = Path.cwd() / "Assets" / "images" / "beam_nobg.png"
-        self.bullet_speed = 7
-        self.bullet_w = 25
-        self.bullet_h = 80
-        self.bullets_amount = 5
         
         # Initialize the game alien settings - the enemy ship
         """
@@ -52,9 +47,7 @@ class Settings:
         self.alien_file = Path.cwd() / "Assets" / "images" / "enemy_ship_nobg.png"
         self.alien_w = 40
         self.alien_h = 40
-        self.fleet_speed = 1
         self.fleet_direction = 1
-        self.fleet_drop_speed = 40
         
         # Initialize the game sound settings
         """
@@ -67,17 +60,16 @@ class Settings:
         filename: explosion-312361.mp3
         """
         self.laser_sound = Path.cwd() / "Assets" / "sound" / "laser-zap-90575.mp3" # Ship firing sound                
-        self.impact_sound = Path.cwd() / "Assets" / "sound" / "explosion-312361.mp3" # Impact sound for bullets hitting aliens
-        
-        self.button_w = 200
-        self.button_h = 50
-        self.button_color = (116, 152, 242)
+        self.impact_sound = Path.cwd() / "Assets" / "sound" / "explosion-312361.mp3" # Impact sound for bullets hitting aliens        
         
         """
         Source URL: https://fonts.google.com/selection?categoryFilters=Appearance:%2FTheme%2FTechno
         filename: Audiowide
         """
         # Initialize the game button settings
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (116, 152, 242)
         self.text_color = (255, 255, 255)
         self.button_font_size = 48
         self.HUD_font_size = 25
@@ -85,4 +77,27 @@ class Settings:
         self.button_font_bold = True
         
         
+    def initialize_dynamic_settings(self) -> None:
+        """
+        Initialize settings that change throughout the game.
+        This method sets the initial values for the ship, bullet, and alien speeds.
+        """
+        self.ship_speed = 5
+        self.starting_ship_amount = 3
         
+        self.bullet_speed = 7
+        self.bullets_amount = 5
+        self.bullet_w = 25
+        self.bullet_h = 80
+        
+        self.fleet_speed = 1
+        self.fleet_drop_speed = 40
+        
+    def increase_difficulty(self) -> None:
+        """
+        Increase the game difficulty settings.
+        This method increases the speed of the ship, bullet, and alien fleet.
+        """
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
